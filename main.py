2600310f1c74d89c2275d77117f2d5be3d6f410c
Osian Smith <osiansmith@me.com>
2021-09-27 17:03:22 +0100

    Added noHelpNeeded, whoToHelp and howManyPeopleNeedHelp

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from discord import message
+from discord.client import Client
 from discord.ext import commands
 import discord
 import os
@@ -32,11 +34,12 @@ async def on_ready():
 async def status(message):
     await message.send('Lab Bot is currently alive - for more status reports, check out osiansmith.com/labbot')
 
+#removes space problems
 @client.command()
 async def gethelp(message):
     await getHelp(message=message)
 
-# Command
+# Gets help - student can call this to request help
 @client.command()
 async def getHelp(message):
     print("adding new student to list")
@@ -47,9 +50,38 @@ async def getHelp(message):
         supportList.append(username)
         await message.send("You have been added to the list - you are number " + str(len(supportList)) )
 
+@client.command()
+async def noHelpNeeded(message):
+    try:
+        supportList.remove(str(message.author))
+        await message.send("You are no longer on the list! To add yourself again use !getHelp")
+    except ValueError:
+        await message.send("You were not on the list")
+
+
+
+
+
+#General Lab demonstrator commands
+@client.command(pass_context=True)
+@commands.has_role("Lab Demonstrator")
+async def whoToHelp(mesage):
+    personToHelp = supportList[0]
+    await message.send("Person next on list is:" + personToHelp + ". They are now removed from list" )
+    supportList.pop(0)
+
+@client.command(pass_context=True)
+@commands.has_role("Lab Demonstrator")
+async def howManyPeopleNeedHelp(mesage):
+    await message.send("Current waiting list length:" + str(len(supportList)))
+
+#Adminy sort of stuff 
+@client.command(pass_context=True)
+@commands.has_role("Lab Demonstrator")
+async def flush(message):
+    await message.send("Flushing Queue")
+    supportList = []
+    await message.send("Success")
 
 client.run(TOKEN)
-
-
-
 
