560db0717b22c5c1394d272b077254931521f4e2
Osian Smith <osiansmith@me.com>
2022-01-10 20:35:27 +0000

    Update to npm and set debug flag correctly

--- main.js
+++ main.js
@@ -5,7 +5,8 @@
  */
 
 // flag whether script is being debugged or not
-const DEBUG = true;
+const DEBUG = false;
+const VERSION = "Labbot 0.6.1 JS"
 
 //Required - dotenv 
 require('dotenv').config()
@@ -208,7 +209,7 @@ client.on('interactionCreate', async interaction => {
     else if (interaction.commandName === 'labbotstatus') {
         var alivemessage = "minutes"
         await interaction.reply('Labbot BETA is currently alive and you are on channel # ' + guildID + "\n " +
-            "Server  has been alive for " + aliveTime + " Minutes \n For more status reports, check out osiansmith.com/labbot \n You are on Labbot 0.6 (JS)");
+            "Server  has been alive for " + aliveTime + " Minutes \n For more status reports, check out osiansmith.com/labbot \n You are on + " + VERSION  + ".");
 
     }
     //gets help 
